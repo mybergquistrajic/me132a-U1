173642ad6be72f9f86e68c2f7e29a7db84e4f7da
My Bergquist Rajic <mybergqvist@gmail.com>
2022-02-03 11:50:14 +0100

    Added function and log

--- U1 (01.02.2022)/Averager/2) Function that adds all the numbers in an array/index.js
+++ U1 (01.02.2022)/Averager/2) Function that adds all the numbers in an array/index.js
@@ -13,16 +13,16 @@ adder_three([2, 1, -2]) should return 1
 
 */
 
-let a1 = [1, 2, 3];
-console.log( adder_three( a1 ) ); // this logs 6 since 1 + 2 + 3 is 6
+// let a1 = [1, 2, 3];
+// console.log( adder_three( a1 ) ); // this logs 6 since 1 + 2 + 3 is 6
 
-function adder_three ( _array ) {
-  let sum = 0;
-  for ( let i = 0; i < 3; i++ ) { 
-    sum = sum + _array[i];
-  }
-  return sum;
-}
+// function adder_three ( _array ) {
+//   let sum = 0;
+//   for ( let i = 0; i < 3; i++ ) { 
+//     sum = sum + _array[i];
+//   }
+//   return sum;
+// }
 
 
 /*
@@ -53,3 +53,14 @@ VIDEO:  Make a video where you explain how adder works line by line. Max 3 minut
 */
 
 
+function adder ( _array ) {
+  let sum = 0;
+  for (let i = 0; i < _array.length; i++) { 
+    sum = sum + _array[i];
+  }
+  return sum;
+}
+
+console.log( adder([5, 2, 1, 7]) );
+console.log( adder([1]) );
+console.log( adder([0]) );
