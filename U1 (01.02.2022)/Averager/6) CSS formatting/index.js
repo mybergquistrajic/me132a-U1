481870abed325029752605295b9d4583c17b4b7d
My Bergquist Rajic <mybergqvist@gmail.com>
2022-02-06 09:17:54 +0100

    Added functions

--- U1 (01.02.2022)/Averager/6) CSS formatting/index.js
+++ U1 (01.02.2022)/Averager/6) CSS formatting/index.js
@@ -8,3 +8,28 @@ You're free to format this as you want, but I expect some effort. See my intial 
 to get a sense of the level of formatting expected.
 
 */
+
+function gridMaker (gridContainer, R, C) {
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+  
+    for (let i = 0; i < R * C; i++) {
+      gridContainer.appendChild(createNumber());
+    }
+
+  }
+  
+  function createNumber () {
+    let number = document.createElement('div');
+    number.innerHTML = randomNumber(100);
+    number.addEventListener("click", function(){number.classList.toggle("selected");});
+
+    function randomNumber (max) {
+      return Math.floor(max * Math.random());
+    }
+  
+    return number;
+  }
+  
+  gridMaker(document.querySelector('#grid'), 4, 6);
+
