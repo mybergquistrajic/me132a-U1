7ba17bcc894b05f8303cca2aa2a8744f83bc3109
My Bergquist Rajic <mybergqvist@gmail.com>
2022-02-06 13:45:50 +0100

    Added link

--- U1 (01.02.2022)/Averager/AveragerApp/index.js
+++ U1 (01.02.2022)/Averager/AveragerApp/index.js
@@ -4,7 +4,7 @@
 // Don't forget to include your CSS-file in the folder
 
 // Include the link to your Github Repository here:
-// Link:
+// Link: https://github.com/mybergquistrajic/me132a-U1.git
 
 
 
